6bf1e869f04b43d12900cba4294c7dea795cf818
josh@jmac <joshashep@gmail.com>
2019-02-24 08:48:38 -0800

    gzip response

--- samples/demo.py
+++ samples/demo.py
@@ -22,6 +22,44 @@ from flask import Flask, render_template, request, jsonify
 from werkzeug import secure_filename
 from scipy.ndimage.morphology import binary_dilation
 
+from flask import after_this_request, request
+from cStringIO import StringIO as IO
+import gzip
+import functools 
+
+def gzipped(f):
+    @functools.wraps(f)
+    def view_func(*args, **kwargs):
+        @after_this_request
+        def zipper(response):
+            accept_encoding = request.headers.get('Accept-Encoding', '')
+
+            if 'gzip' not in accept_encoding.lower():
+                return response
+
+            response.direct_passthrough = False
+
+            if (response.status_code < 200 or
+                response.status_code >= 300 or
+                'Content-Encoding' in response.headers):
+                return response
+            gzip_buffer = IO()
+            gzip_file = gzip.GzipFile(mode='wb', 
+                                      fileobj=gzip_buffer)
+            gzip_file.write(response.data)
+            gzip_file.close()
+
+            response.data = gzip_buffer.getvalue()
+            response.headers['Content-Encoding'] = 'gzip'
+            response.headers['Vary'] = 'Accept-Encoding'
+            response.headers['Content-Length'] = len(response.data)
+
+            return response
+
+        return f(*args, **kwargs)
+
+    return view_func
+
 # COCO Class names
 # Index of the class in the list is its ID. For example, to get ID of
 # the teddy bear class, use: class_names.index('teddy bear')
@@ -128,6 +166,7 @@ def upload_page():
     return render_template('index4.html')
     
 @app.route('/uploader', methods = ['POST'])
+@gzipped
 def upload_file():
     print("/uploader called")
     if request.method == 'POST':
