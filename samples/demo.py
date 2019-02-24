75786675904e43e9b071ae998a05e64679a5c0b2
josh@jmac <joshashep@gmail.com>
2019-02-24 02:14:54 -0800

    hmm thread safety...

--- samples/demo.py
+++ samples/demo.py
@@ -14,65 +14,68 @@ import matplotlib.pyplot as plt
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 
-# Root directory of the project
-ROOT_DIR = os.path.abspath("../")
+def build_model():
+    # Root directory of the project
+    ROOT_DIR = os.path.abspath("../")
 
-# Import Mask RCNN
-sys.path.append(ROOT_DIR)  # To find local version of the library
-from mrcnn import utils
-import mrcnn.model as modellib
-from mrcnn import visualize
-# Import COCO config
-sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
-import coco
+    # Import Mask RCNN
+    sys.path.append(ROOT_DIR)  # To find local version of the library
+    from mrcnn import utils
+    import mrcnn.model as modellib
+    from mrcnn import visualize
+    # Import COCO config
+    sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
+    import coco
 
-#%matplotlib inline 
+    #%matplotlib inline 
 
-# Directory to save logs and trained model
-MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+    # Directory to save logs and trained model
+    MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
-# Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
-# Download COCO trained weights from Releases if needed
-if not os.path.exists(COCO_MODEL_PATH):
-    utils.download_trained_weights(COCO_MODEL_PATH)
+    # Local path to trained weights file
+    COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+    # Download COCO trained weights from Releases if needed
+    if not os.path.exists(COCO_MODEL_PATH):
+        utils.download_trained_weights(COCO_MODEL_PATH)
 
-# Directory of images to run detection on
-IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+    # Directory of images to run detection on
+    IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
-class InferenceConfig(coco.CocoConfig):
-    # Set batch size to 1 since we'll be running inference on
-    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
+    class InferenceConfig(coco.CocoConfig):
+        # Set batch size to 1 since we'll be running inference on
+        # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
 
-config = InferenceConfig()
-config.display()
+    config = InferenceConfig()
+    config.display()
 
 
-# Create model object in inference mode.
-model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+    # Create model object in inference mode.
+    model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
 
-# Load weights trained on MS-COCO
-print("Loading weights for model...")
-model.load_weights(COCO_MODEL_PATH, by_name=True)
+    # Load weights trained on MS-COCO
+    print("Loading weights for model...")
+    model.load_weights(COCO_MODEL_PATH, by_name=True)
+    return model
 
 
 app = Flask(__name__)
 
 @app.route('/upload_page')
 def upload_page():
-   return render_template('upload_page.html')
+    return render_template('upload_page.html')
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
 		
 if __name__ == '__main__':
-   app.run(debug = True)
+    model = build_model()
+    app.run(debug = True)
 
 '''
 
