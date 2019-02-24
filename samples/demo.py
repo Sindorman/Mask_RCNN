c4e0fe68ac1360bce3495f5b613fab31aa77c8f6
josh@jmac <joshashep@gmail.com>
2019-02-24 03:21:19 -0800

    let's try this

--- samples/demo.py
+++ samples/demo.py
@@ -16,6 +16,25 @@ import matplotlib.pyplot as plt
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
 def build_model():
     # Root directory of the project
     ROOT_DIR = os.path.abspath("../")
@@ -61,37 +80,57 @@ def build_model():
     model.load_weights(COCO_MODEL_PATH, by_name=True)
     return model
 
+if __name__ == '__main__':
+    #interesting ...
+    model = None
 
 app = Flask(__name__)
 USER_IMG_DIR = "user_imgs/"
 
-class MaskGenerator():
-    def __init__(self):
-        self.membervar = 5
-        app.run(debug = True, use_reloader=False, host="0.0.0.0", port=80) # port 80 means sudo only :/
+def get_masks(ifname):
+    image = skimage.io.imread(ifname)
 
+    # Run detection
+    print("detecting objects...")
+    start_time = time()
+    results = model.detect([image], verbose=1)
+    end_time = time()
+    secs_elapsed = end_time - start_time
+    print("detection took {:0.2f} seconds.".format(secs_elapsed))
 
-    @app.route('/upload_page')
-    def upload_page(self):
-        print("/upload_page called in A CLASS")
-        return render_template('upload_page.html')
-        
-    @app.route('/uploader', methods = ['POST'])
-    def upload_file(self):
-        print("/uploader called")
-        if request.method == 'POST':
-            f = request.files['file']
-            rand_uuid = uuid.uuid4().hex
-            ofname = path.join(USER_IMG_DIR, rand_uuid + "_" + secure_filename(f.filename))
-            print("writing to file '{}' ...".format(ofname))
-            f.save(ofname)
-            return 'file uploaded successfully'
+    r = results[0]
+    #visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+    #                        class_names, r['scores'])
+
+    print("rois (shape={}): {}".format(r['rois'].shape, r['rois']))
+    print("masks (shape={}): {}".format(r['masks'].shape, r['masks']))
+    print("class_ids (shape={}): {}".format(r['class_ids'].shape, r['class_ids']))
+    print("scores (shape={}): {}".format(r['scores'].shape, r['scores']))
+    print("STATIC class_names (len={}): {}".format(len(class_names), class_names))
+
+@app.route('/upload_page')
+def upload_page():
+    print("/upload_page called in A CLASS")
+    return render_template('upload_page.html')
+    
+@app.route('/uploader', methods = ['POST'])
+def upload_file():
+    print("/uploader called")
+    if request.method == 'POST':
+        f = request.files['file']
+        rand_uuid = uuid.uuid4().hex
+        ofname = path.join(USER_IMG_DIR, rand_uuid + "_" + secure_filename(f.filename))
+        print("writing to file '{}' ...".format(ofname))
+        f.save(ofname)
+
+        get_masks(ofname)
+        return 'file uploaded successfully'
 		
 if __name__ == '__main__':
-    #model = build_model()
+    model = build_model()
 
     # use_reloader on makes us load the model twice (this is slow and bad)
-    maskGenerator = MaskGenerator()
+    app.run(debug = True, use_reloader=False, host="0.0.0.0", port=80) # port 80 means sudo only :/
 '''
 
 # COCO Class names
